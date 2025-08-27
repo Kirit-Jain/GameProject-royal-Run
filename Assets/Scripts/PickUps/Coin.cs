c53b2a542dd9337e49e71ec5181b1dd997338f49
Kirit-Jain <kirit.ajain@gmail.com>
2025-08-27 14:52:16 +0530

    Set up Scoreboard

--- Assets/Scripts/PickUps/Coin.cs
+++ Assets/Scripts/PickUps/Coin.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Coin : PickUp
 {
+    [SerializeField] int scoreAmount = 100;
+    ScoreboardManager scoreboardManager;
+
+
+    void Start()
+    {
+        scoreboardManager = FindFirstObjectByType<ScoreboardManager>();
+        if (scoreboardManager == null)
+            Debug.LogError("No Scoreboard Manager found in scene, please add one");
+    }
     protected override void OnPickUp()
     {
-        Debug.Log("Add 100 points");
+        scoreboardManager.IncreaceScore(scoreAmount);
     }
 }
--- Assets/Scripts/Player/ScoreboardManager.cs
+++ Assets/Scripts/Player/ScoreboardManager.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
 using TMPro;
+using System.Data;
 
 public class ScoreboardManager : MonoBehaviour
 {
     [SerializeField] TMP_Text scoreText;
 
-
     int score = 0;
 
-    public void IncreaseScore(int amount);
+    public void IncreaceScore(int amount)
     {
         score += amount;
         scoreText.text = score.ToString();
     }
+
 }
